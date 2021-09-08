ec5df72721ce44f91e373485b5f8a8f71330ead4
BrunoClevenot <brunoxlevil@hotmail.com>
2021-09-08 10:13:24 +0200

    Amélioration du js pour les pages index et produit (gestion des erreurs)

--- Static/js/index.js
+++ Static/js/index.js
@@ -10,26 +10,44 @@ fetch("http://localhost:3000/api/cameras")
         };
     })
     .then(function(value){
-        value.forEach(camera => {
-            if ("content" in document.createElement("template")) {
-                let template = document.querySelector("#template-card-box");
-                let article = document.querySelector("article");
-                let clone = document.importNode(template.content, true);
-                let p = clone.querySelectorAll("p");
-                let picture = clone.querySelector("img");
-                let a =clone.querySelector("a");
-                let texteReference = "produit.html?id="+camera._id;
+        let nombre = value.length;
+        let article = document.querySelector("article");
 
-                p[0].textContent = "Nom : " + camera.name;
-                p[1].textContent = "Description : " + camera.description;
-                p[2].textContent = "Prix : " + camera.price/100 +" €";
-                picture.src=camera.imageUrl
-                a.href= texteReference
-                article.appendChild(clone);
-            }
-        });
+        console.log(nombre);
+        
+
+        if (nombre> 0){
+            value.forEach(camera => {
+                if ("content" in document.createElement("template")) {
+                    let template = document.querySelector("#template-card-box");
+                    let clone = document.importNode(template.content, true);
+                    clone.querySelector("img").src=camera.imageUrl;
+                    let texteReference = "produit.html?id="+camera._id;
+                    clone.querySelector("a").href=texteReference;
+                    clone.querySelector(".slot-nom").textContent = camera.name;
+                    clone.querySelector(".slot-description").textContent = camera.description;
+                    clone.querySelector(".slot-prix").textContent = camera.price/100;
+                    article.appendChild(clone);
+                }
+            });
+        }else if (nombre===0){
+            let template = document.querySelector("#template-error-index");
+            let clone = document.importNode(template.content, true);
+            clone.querySelector(".slot-noData-1").textContent="Désolé, aucun produit n'a été trouvé dans la base.";
+            clone.querySelector(".slot-noData-2").textContent="Nous vous invitons à revenir ultérieurement."
+            article.appendChild(clone);
+        }
     })
     .catch(function(err){
-        console.log("Erreur :" + error);
+        let template = document.querySelector("#template-error-index");
+        let clone = document.importNode(template.content, true);
+        if (err=="TypeError: value is undefined"){
+            clone.querySelector(".slot-noData-1").textContent="Désolé, aucun produit n'a été trouvé dans la base.";
+            clone.querySelector(".slot-noData-2").textContent="Nous vous invitons à revenir ultérieurement.";
+        }else{
+            clone.querySelector(".slot-noData-1").textContent="Désolé, le serveur ne répond pas.";
+            clone.querySelector(".slot-noData-2").textContent="Nous vous invitons à revenir ultérieurement.";
+        }
+        console.log("Erreur :" + err);
     })
 	
--- Static/js/produit.js
+++ Static/js/produit.js
@@ -6,17 +6,14 @@ const url = window.location.href;
 const urlObj = new URL(url);
 const idTrouvee = urlObj.searchParams.get("id");
 
-//Définition du prix unitaire
-let prixUnitaire=0;
-
 // Fonction de calcul du prix global
-function globalPrice(){
+function globalPrice(ev,value){
     let element = document.getElementById("nombre");
     let nbre = element.value;
     console.log(nbre);
-    let prixTotal = prixUnitaire * nbre;
-    console.log(prixUnitaire);
-    document.getElementById("prix-total").innerText="Prix total : " + prixTotal +" €";
+    console.dir(ev)
+    let prixTotal = value.price/100 * nbre;
+    document.getElementById("prix-total").innerText=prixTotal;
 }
 
 // Récupération de l'article via id
@@ -27,31 +24,46 @@ fetch("http://localhost:3000/api/cameras/" + idTrouvee)
         };
     })
     .then(function(value){
-        prixUnitaire=value.price/100;
-        console.log(prixUnitaire);
-        console.log(value);
-        console.log(value.length)
-        console.log(value.imageUrl);
-        document.getElementById("nom").innerText="Nom : " + value.name;
-        document.getElementById("description").innerText="Description : " + value.description;
-        document.getElementById("image").setAttribute("src",value.imageUrl);
-        let opt = document.getElementById("lentilles");
-        if (value.lenses.length>0){
-            for (let i = 0; i < value.lenses.length; i++) {
-                let option = document.createElement("option"); 
-                option.text=value.lenses[i];
-                option.setAttribute("value",value.lenses[i]);
-                if (i ===0){
-                    opt.append(option);
-                    option.selected=true;
-                }else{            
-                    opt.append(option);
+        if ("content" in document.createElement("template")) {
+            prixUnitaire=value.price/100;
+            console.log(prixUnitaire);
+            console.log(value);
+            console.log(value.imageUrl);
+            let article = document.querySelector("article");
+            let template = document.querySelector("#template-card-box-produit");
+            let clone = document.importNode(template.content, true);
+            clone.querySelector("img").src=value.imageUrl;
+            clone.querySelector(".slot-nom").textContent=value.name;
+            clone.querySelector(".slot-description").textContent = value.description;
+            if (value.lenses.length>0){
+                for (let i = 0; i < value.lenses.length; i++) {
+                    opt=clone.querySelector("#lentilles")
+                    option=document.createElement("option");
+                    option.text=value.lenses[i];
+                    option.value=value.lenses[i]
+                    if (i ===0){
+                        opt.append(option);
+                        option.selected=true;
+                    }else{            
+                        opt.append(option);
+                    }
                 }
             }
+            clone.querySelector(".slot-prix").textContent = value.price/100;
+            clone.querySelector(".slot-prix-total").textContent=value.price/100;
+            clone.querySelector("#nombre").addEventListener("change",(ev)=>globalPrice(ev,value));
+            article.appendChild(clone);
         }
-        document.getElementById("prix-unitaire").innerText="Prix unitaire : " + value.price/100 +" €";
-        document.getElementById("prix-total").innerText="Prix total : " + value.price/100 +" €";
+        
     })
     .catch(function(err){
-        console.log("Erreur :" + error);
+        if (err=="TypeError: value is undefined"){
+            let article = document.querySelector("article");
+            let template = document.querySelector("#template-error-produit");
+            let clone = document.importNode(template.content, true);
+            clone.querySelector(".slot-noData-1").textContent="Désolé, aucun produit n'a été trouvé dans la base.";
+            clone.querySelector(".slot-noData-2").textContent="Nous vous invitons à revenir ultérieurement."
+            article.appendChild(clone);
+        }
+        console.log("Erreur : "+ err);
     })
